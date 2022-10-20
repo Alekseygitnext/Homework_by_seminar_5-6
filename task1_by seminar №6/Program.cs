c030bbe3715002f25964c1fba7e2a5261c5ab8da
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-20 20:20:19 +1000

    Задача 1 к семинару № 6

---
+++ task1_by seminar №6/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двухмерный массив размером m*n заполненный вещественными числами
+
+Console.WriteLine("введите количество строк");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("введите количество столбцов");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+double[,] numbers = new double[rows, columns];
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-1000, 1000)) /100;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
