a2a44c9f910f0a9827865944d3ad522cc2bed7a8
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-20 20:50:42 +1000

    Задача 3 к семинару № 6

---
+++ task3_by seminar №6/Program.cs
@@ -0,0 +1,51 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадрат
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Replace(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            if ((m % 2 == 0) && (n % 2 == 0))
+            {
+                array[m, n] = array[m, n] * array[m, n];
+            }
+        }
+    }
+}
+    
+Console.Write("Введите число m :  ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+Console.Write("Введите число n :  ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+int[,] array = new int[m, n];
+FillArray(array);
+Console.WriteLine("Оригинал массива :  ");
+PrintArray(array);
+Console.WriteLine();
+Replace(array);
+Console.WriteLine("Массив с заменёнными элементами :  ");
+PrintArray(array);
